41f263155eb07f0835640cbcdc33980c49f0ad2b
Aaron Meurer <asmeurer@gmail.com>
2019-12-17 15:58:15 -0700

    Add Day 2 solutions

---
+++ 2019/day2_2.py
@@ -0,0 +1,12 @@
+from day2_1 import prog, input
+
+if __name__ == '__main__':
+    def test():
+        for noun in range(100):
+            for verb in range(100):
+                if prog(input, noun=noun, verb=verb)[0] == 19690720:
+                    return (noun, verb)
+
+    noun, verb = test()
+
+    print(noun*100 + verb)
