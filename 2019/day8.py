37dd039cfcb87cf46303014d595cbe822c9df29f
Aaron Meurer <asmeurer@gmail.com>
2019-12-17 23:12:50 -0700

    Complete day 8 part 2

--- 2019/day8.py
+++ 2019/day8.py
@@ -18,9 +18,38 @@ def find_solution(a):
     return np.sum(a[min_i] == 1)*np.sum(a[min_i] == 2)
 
 
+block = '█'
+def inverse(a):
+    return '\x1b[7m' + a + '\x1b[0m'
+
+black = block
+white = inverse(block)
+
+def show_image(a):
+    layers, rows, columns = a.shape
+    for r in range(rows):
+        for c in range(columns):
+            for l in range(layers):
+                if a[l, r, c] == 0:
+                    print(black, end='')
+                    break
+                elif a[l, r, c] == 1:
+                    print(white, end='')
+                    break
+        print()
+
 
 with open('day8-input') as f:
     input = f.read()
 
+test_image = '0222112222120000'
+
 if __name__ == '__main__':
-    print(find_solution(split_layers(input, 6, 25)))
+    a = split_layers(test_image, 2, 2)
+    print("test image")
+    show_image(a)
+
+    a = split_layers(input, 25, 6)
+    print(find_solution(a))
+    print("Day 8 image")
+    show_image(a)
