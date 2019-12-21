c65555002d418f2bbfc87c770e42ff9ac9e7f2d8
Aaron Meurer <asmeurer@gmail.com>
2019-12-20 19:50:37 -0700

    Start day 12 part 2 based on day 12 part 1 with numba jit

---
+++ 2019/day12_2.py
@@ -0,0 +1,87 @@
+from itertools import combinations
+
+import numpy as np
+from numba import njit
+
+combos = np.array(list(combinations(range(4), 2)))
+
+@njit
+def step(P, V):
+    delta = np.zeros((V.shape), dtype=np.int64)
+    for c in range(combos.shape[0]):
+        i, j = combos[c][0], combos[c][1]
+        # Apply gravity
+        for coord in range(3):
+            if P[i][coord] < P[j][coord]:
+                delta[i][coord] += 1
+                delta[j][coord] -= 1
+            elif P[i][coord] > P[j][coord]:
+                delta[i][coord] -= 1
+                delta[j][coord] += 1
+
+    V = V + delta
+    P = P + V
+    return P, V
+
+@njit
+def run_steps(P, n, print_=False):
+    V = np.zeros(P.shape, dtype=np.int64)
+    for i in range(n):
+        P, V = step(P, V)
+        # if print_:
+        #     print("Iteration", i)
+        #     print("Positions\n", P)
+        #     print("Velocities\n", V)
+        #     print()
+    return P, V
+
+def parse_input(I):
+    I = I.strip().splitlines()
+    P = np.zeros((len(I), 3), dtype=np.int64)
+    for i in range(len(I)):
+        line = I[i].strip('<>').split(', ')
+        coords = [int(i[2:]) for i in line]
+        P[i] = coords
+
+    return P
+
+# @njit
+def compute_energy(P, V):
+    return np.sum(np.sum(abs(P), axis=1)*np.sum(abs(V), axis=1))
+
+test_input1 = """
+<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>
+"""
+
+test_input2 = """
+<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>
+"""
+
+input = """
+<x=16, y=-8, z=13>
+<x=4, y=10, z=10>
+<x=17, y=-5, z=6>
+<x=13, y=-3, z=0>
+"""
+
+if __name__ == '__main__':
+    print("Day 12, part, test 1")
+    P = parse_input(test_input1)
+    P, V = run_steps(P, 10, print_=True)
+    print(compute_energy(P, V))
+
+    print("Day 12, part 1, test 2")
+    P = parse_input(test_input2)
+    P, V = run_steps(P, 100, print_=True)
+    print(compute_energy(P, V))
+
+    print("Day 12, part 1")
+    P = parse_input(input)
+    P, V = run_steps(P, 1000)
+    print(compute_energy(P, V))
