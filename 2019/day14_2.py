9c67fc7880a1d3d3f336ae48c532470dffb6d6c6
Aaron Meurer <asmeurer@gmail.com>
2019-12-22 20:23:59 -0700

    Fix the logic for day 14 part 2
    It's still to slow to finish, though.

--- 2019/day14_2.py
+++ 2019/day14_2.py
@@ -1,14 +1,23 @@
 from collections import defaultdict
 
-from day14_1 import parse_input, FUEL, ORE, produce
+from day14_1 import (parse_input, FUEL, ORE, produce as _produce, test_input_3,
+                     test_input_4, test_input_5, input)
+
+def produce(out, rxns, available):
+    available = available.copy()
+    _produce(out, rxns, available)
+    return available
 
 def get_fuel_for_ore(rxns, ore):
     available = defaultdict(int)
-    produce(FUEL, rxns, available)
+    available = produce(FUEL, rxns, available)
+    print("Finding base multiple")
+    available = find_base_multiple(available, rxns)
     doubles = [available]
     while available[ORE] < ore // 2:
         available = double(available)
         doubles.append(available)
+    print("Adding doubles")
     while available[ORE] < ore:
         for d in reversed(doubles):
             if d[ORE] + available[ORE] <= ore:
@@ -17,10 +26,25 @@ def get_fuel_for_ore(rxns, ore):
                 break
         else:
             break
+    print("Doing final production")
+    available0 = available.copy()
     while available[ORE] < ore:
         # print(available[ORE], available[FUEL])
-        produce(FUEL, rxns, available)
-    return available[FUEL]
+        available0, available = available, produce(FUEL, rxns, available)
+    return available0[FUEL]
+
+def find_base_multiple(available, rxns):
+    while True:
+        for i in available:
+            if i in [FUEL, ORE]:
+                continue
+            if available[i] != 0:
+                break
+        else:
+            break
+        available = produce(FUEL, rxns, available)
+        print(available[FUEL])
+    return available
 
 def double(available):
     return {k: v*2 for k, v in available.items()}
@@ -28,79 +52,23 @@ def double(available):
 def add(a, b):
     return {k: a[k] + b[k] for k in a}
 
-test_input_1 = """
-10 ORE => 10 A
-1 ORE => 1 B
-7 A, 1 B => 1 C
-7 A, 1 C => 1 D
-7 A, 1 D => 1 E
-7 A, 1 E => 1 FUEL
-"""
+# def test(rxns):
+#     available = defaultdict(int)
+#     produce(FUEL, rxns, available)
+#     d = double(available)
+#     a = add(available, available)
+#     produce(FUEL, rxns, available)
+#     assert d == a
+#     print(d)
+#     print(dict(available))
+#     assert d == available
 
-test_input_2 = """
-9 ORE => 2 A
-8 ORE => 3 B
-7 ORE => 5 C
-3 A, 4 B => 1 AB
-5 B, 7 C => 1 BC
-4 C, 1 A => 1 CA
-2 AB, 3 BC, 4 CA => 1 FUEL
-"""
-
-test_input_3 = """
-157 ORE => 5 NZVS
-165 ORE => 6 DCFZ
-44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
-12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
-179 ORE => 7 PSHF
-177 ORE => 5 HKGWZ
-7 DCFZ, 7 PSHF => 2 XJWVT
-165 ORE => 2 GPVTF
-3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT
-"""
-
-test_input_4 = """
-2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
-17 NVRVD, 3 JNWZP => 8 VPVL
-53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
-22 VJHF, 37 MNCFX => 5 FWMGM
-139 ORE => 4 NVRVD
-144 ORE => 7 JNWZP
-5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
-5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
-145 ORE => 6 MNCFX
-1 NVRVD => 8 CXFTF
-1 VJHF, 6 MNCFX => 4 RFSQX
-176 ORE => 6 VJHF
-"""
-
-test_input_5 = """
-171 ORE => 8 CNZTR
-7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
-114 ORE => 4 BHXH
-14 VRPVC => 6 BMBT
-6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
-6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
-15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
-13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
-5 BMBT => 4 WPTQ
-189 ORE => 9 KTJDG
-1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
-12 VRPVC, 27 CNZTR => 2 XDBXC
-15 KTJDG, 12 BHXH => 5 XCVML
-3 BHXH, 2 VRPVC => 7 MZWV
-121 ORE => 7 VRPVC
-7 XCVML => 6 RJRHP
-5 BHXH, 4 VRPVC => 5 LTCX
-"""
-
-with open('day14-input') as f:
-    input = f.read()
-
-if __name__ == '__main__':
+def main():
     print("Day14, Part 2, test input 3")
     print("Should give 82892753")
-    print(get_fuel_for_ore(parse_input(test_input_3), 1000000000000))
+    rxns = parse_input(test_input_3)
+    # test(rxns)
+    print(get_fuel_for_ore(rxns, 1000000000000))
 
     print("Day14, Part 2, test input 4")
     print("Should give 5586022")
@@ -109,3 +77,9 @@ if __name__ == '__main__':
     print("Day14, Part 2, test input 5")
     print("Should give 460664")
     print(get_fuel_for_ore(parse_input(test_input_5), 1000000000000))
+
+    print("Day14, Part 2")
+    print(get_fuel_for_ore(parse_input(input), 1000000000000))
+
+if __name__ == '__main__':
+    main()
