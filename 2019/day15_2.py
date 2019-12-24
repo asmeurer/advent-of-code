b46be1caafb83af8beb6d836ea55d110e7173083
Aaron Meurer <asmeurer@gmail.com>
2019-12-23 18:21:26 -0700

    WIP for day 15 part 2

--- 2019/day15_2.py
+++ 2019/day15_2.py
@@ -4,7 +4,9 @@ import sys
 
 import numpy as np
 
-from day13 import prog, empty, wall, block
+from day13 import prog
+
+from day15_1 import moves, print_board, luby, input, create_tree
 
 def run(input, p=None, max_iter=sys.maxsize, print_=False):
     in_ = []
@@ -33,119 +35,46 @@ def run(input, p=None, max_iter=sys.maxsize, print_=False):
                 A[tuple(pos + moves[move])] = 1
             elif status == 1:
                 # Moved
-                A[tuple(pos + moves[move])] = -1
                 pos += moves[move]
+                if tuple(pos) != (0, 0):
+                    A[tuple(pos)] = -1
             elif status == 2:
                 # Moved and found oxygen
-                A[tuple(pos + moves[move])] = 2
-                return A, pos
-
-            if print_:
+                pos += moves[move]
+                A[tuple(pos)] = 2
+            if print_ and i % 100 == 0:
                 print_board(A, pos)
+            if full(A):
+                return A, pos
         except StopIteration:
             return A, pos
 
     raise RuntimeError("Did not find a solution")
 
-# From https://timvieira.github.io/blog/post/2019/09/06/the-restart-acceleration-trick-a-cure-for-the-heavy-tail-of-wasted-time/
-def luby():
-    def h(k):
-        if k == 0:
-            yield 1
-        else:
-            for j in range(k):
-                yield from h(j)
-            yield 2 ** k
-
-    i = 0
-    while True:
-        yield from h(i)
-        i += 1
-
-moves = {
-    1: np.array([0, -1]), # north
-    2: np.array([0, 1]), # south
-    3: np.array([1, 0]), # west
-    4: np.array([-1, 0]), # east
-}
-
-bot = '🤖'
-oxygen = 'O₂'
-start = 'ⓧ '
-
-D = {
-    -2: start,   # Start
-    -1: 2*block, # Discovered empty
-    0: 2*empty,  # Undiscovered
-    1: 2*wall,   # Wall
-    2: oxygen,   # Oxygen
-}
-
-def print_board(A, pos):
-    min_x = min(list(A) + [pos], key=lambda i: i[0])[0]
-    max_x = max(list(A) + [pos], key=lambda i: i[0])[0]
-    min_y = min(list(A) + [pos], key=lambda i: i[1])[1]
-    max_y = max(list(A) + [pos], key=lambda i: i[1])[1]
-    S = ''
+def full(A):
+    min_x = min(list(A), key=lambda i: i[0])[0]
+    max_x = max(list(A), key=lambda i: i[0])[0]
+    min_y = min(list(A), key=lambda i: i[1])[1]
+    max_y = max(list(A), key=lambda i: i[1])[1]
     for y in range(min_y, max_y + 1):
         for x in range(min_x, max_x + 1):
-            if (x, y) == tuple(pos):
-                S += bot
-            else:
-                S += D[A[x, y]]
-        S += '\n'
-    print(S)
-    print('-------------------')
-    # import time
-    # time.sleep(0.1)
-
-def retry(input):
-    for r in luby():
-        try:
-            iters = r*500
-            print("Trying with", iters, "max iterations")
-            A, pos = run(input, max_iter=iters, print_=False)
-            return A, pos
-        except RuntimeError:
-            pass
-
-def create_tree(A):
-    tree = defaultdict(set)
-    # Make sure (0, 0) is included as the base
-    add_to_tree(A, tree, (0, 0))
-
-    for pos in A:
-        add_to_tree(A, tree, pos)
-
-    return tree
-
-def add_to_tree(A, tree, pos):
-    for move in moves.values():
-        pos2 = tuple(np.array(pos) + move)
-        if pos2 in A and A[pos2] in [-1, 2] and pos not in tree[pos2]:
-            tree[pos].add(pos2)
-
-def dfs(tree, path):
-    for item in tree[path[-1]]:
-        yield path + [item]
-        yield from dfs(tree, path + [item])
-
-with open('day15-input') as f:
-    input = f.read()
+            if A[x, y] == 0:
+                # Ignore spaces that are completely surrounded by wall
+                for move in moves.values():
+                    x2, y2 = tuple(np.array([x, y]) + move)
+                    if x2 < min_x or x2 > max_x or y2 < min_y or y2 > max_y:
+                        continue
+                    if A[x2, y2] != 1:
+                        return False
+                return False
+    return True
 
 def main():
-    A, pos = retry(input)
+    A, pos = run(input, print_=True)
     print_board(A, pos)
-    tree = create_tree(A)
     oxygen = [i for i in A if A[i] == 2][0]
-    for path in dfs(tree, [(0, 0)]):
-        if path[-1] == oxygen:
-            # print(path)
-            print(len(path) - 1, "moves")
-            break
-    else:
-        print("Error: couldn't find path")
-
+    tree = create_tree(A, oxygen)
+    print(tree)
 
 if __name__ == '__main__':
     main()
