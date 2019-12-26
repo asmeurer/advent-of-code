b5d5d57ad4d2eebdf197bbdba98bd560fcc46780
Aaron Meurer <asmeurer@gmail.com>
2019-12-25 21:40:40 -0700

    Add day 17 part 2

--- 2019/day17.py
+++ 2019/day17.py
@@ -1,14 +1,20 @@
+import time
+
 import numpy as np
 
 from day13 import prog
 
-def run(input, p=None):
-    in_ = []
-    p = p or prog(input, in_)
+def run(input, in_=None, print_=False):
+    in_ = in_ or []
+    p = prog(input, in_)
     out = ''
     while True:
         try:
             out += chr(next(p))
+            if print_:
+                print(out[-1], end='')
+                if out[-2:] == '\n\n':
+                    time.sleep(0.1)
         except StopIteration:
             return out
 
@@ -58,20 +64,44 @@ test_input = """
 ..#####...^..
 """
 
+main = 'A                B             B             A                B             C                A                C                B             C'
+path = 'L,4,L,6,L,8,L,12 L,8,R,12,L,12 L,8,R,12,L,12 L,4,L,6,L,8,L,12 L,8,R,12,L,12 R,12,L,6,L,6,L,8 L,4,L,6,L,8,L,12 R,12,L,6,L,6,L,8 L,8,R,12,L,12 R,12,L,6,L,6,L,8'
+_main = ','.join(main.split())
+A = path.split()[0]
+B = path.split()[1]
+C = path.split()[-1]
+
+assert len(A) <= 20
+assert len(B) <= 20
+assert len(C) <= 20
+
 def main():
     print("Day 17 part 1 test input")
-    A = toarray(test_input)
-    intersections = find_intersections(A)
+    Arr = toarray(test_input)
+    intersections = find_intersections(Arr)
     print("Should return 76")
     print(alignment(intersections))
 
     print("Day 17 part 1 test input")
     out = run(input)
     print(out)
-    A = toarray(out)
-    intersections = find_intersections(A)
+    Arr = toarray(out)
+    intersections = find_intersections(Arr)
     print(alignment(intersections))
 
+    print("Day 17 part 2")
+    input2 = '2' + input[1:]
+    in_ = []
+    for cmd in [_main, A, B, C]:
+        for c in cmd:
+            in_.append(ord(c))
+        in_.append(ord('\n'))
+    in_.extend([ord('y'), ord('\n')])
+
+    print("Input:", ''.join([chr(i) for i in in_]))
+    out = run(input2, in_, print_=True)
+    print()
+    print(ord(out[-1]))
 
 if __name__ == '__main__':
     main()
