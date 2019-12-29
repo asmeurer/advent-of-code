fc1335ae5a8c6eb5336d55a65991bfeb2c9a272e
Aaron Meurer <asmeurer@gmail.com>
2019-12-29 13:32:24 -0700

    Better solution for day 22 part 1

--- 2019/day22_2.py
+++ 2019/day22_2.py
@@ -1,30 +1,27 @@
-from sympy import mod_inverse
+from sympy import symbols, lambdify
 
 import numpy as np
 
 import re
 
 def parse_input(I, N):
-    a = np.arange(N)
-    b = a.copy()
+    expr = symbols('x')
+
     for line in I.strip().splitlines():
         m = re.match(r'deal with increment (.*)', line)
         if m:
-            b = b[int(mod_inverse(int(m.group(1)), N))*a % N]
-            assert len(np.unique(b)) == N, line
+            expr = expr*int(m.group(1))
             continue
         m = re.match(r'deal into new stack', line)
         if m:
-            b = b[::-1]
-            assert len(np.unique(b)) == N, line
+            expr = -expr - 1
             continue
         m = re.match(r'cut (.*)', line)
         if m:
-            b = b[(a + int(m.group(1))) % N]
-            assert len(np.unique(b)) == N, line
+            expr = expr - int(m.group(1))
             continue
         raise ValueError("Unrecognized input: %s" % line)
-    return b
+    return expr % N
 
 test_input_1 = """
 deal with increment 7
@@ -32,17 +29,23 @@ deal into new stack
 deal into new stack
 """
 
+test_answer_1 = np.array([int(i) for i in '0 3 6 9 2 5 8 1 4 7'.split()])
+
 test_input_2 = """
 cut 6
 deal with increment 7
 deal into new stack
 """
 
+test_answer_2 = np.array([int(i) for i in '3 0 7 4 1 8 5 2 9 6'.split()])
+
 test_input_3 = """
 deal with increment 7
 deal with increment 9
 cut -2
 """
+
+test_answer_3 = np.array([int(i) for i in '6 3 0 7 4 1 8 5 2 9'.split()])
 
 test_input_4 = """
 deal into new stack
@@ -61,34 +64,31 @@ with open('day22-input') as f:
     input = f.read()
 
 def main():
+    x = symbols('x')
+
     print("Day 22, test input 1")
-    res = parse_input(test_input_1, 10)
-    print("Should be 0 3 6 9 2 5 8 1 4 7")
-    print(res)
+    expr = parse_input(test_input_1, 10)
+    print(expr)
+    print(lambdify(x, expr)(test_answer_1))
+    print(lambdify(x, expr)(2))
     print()
 
     print("Day 22, test input 2")
-    res = parse_input(test_input_2, 10)
-    print("Should be 3 0 7 4 1 8 5 2 9 6")
-    print(res)
+    expr = parse_input(test_input_2, 10)
+    print(expr)
+    print(lambdify(x, expr)(test_answer_2))
     print()
 
     print("Day 22, test input 3")
-    res = parse_input(test_input_3, 10)
-    print("Should be 6 3 0 7 4 1 8 5 2 9")
-    print(res)
-    print()
-
-    print("Day 22, test input 4")
-    res = parse_input(test_input_4, 10)
-    print("Should be 9 2 5 8 1 4 7 0 3 6")
-    print(res)
+    expr = parse_input(test_input_3, 10)
+    print(expr)
+    print(lambdify(x, expr)(test_answer_3))
     print()
 
     print("Day 22, part 1")
-    res = parse_input(input, 10007)
-    print(res)
-    print(np.where(res == 2019)[0][0])
+    expr = parse_input(input, 10007)
+    print(expr)
+    print(lambdify(x, expr)(2019))
 
 if __name__ == '__main__':
     main()
