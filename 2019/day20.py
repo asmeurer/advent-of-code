b2f027d4fbecc16600fdfedf6ce0dbb0d60dc98d
Aaron Meurer <asmeurer@gmail.com>
2019-12-29 15:13:05 -0700

    Start day 20

--- 2019/day20.py
+++ 2019/day20.py
@@ -1,3 +1,9 @@
+import numpy as np
+
+def asarray(I):
+    lines = I.splitlines()
+    return np.array(lines, "S").view("S1").reshape((len(lines), -1))
+
 with open('day20-input') as f:
     input = f.read()
 
