b8339d871369715590c6c62810092355eb142086
Aaron Meurer <asmeurer@gmail.com>
2021-12-11 21:57:36 -0700

    Add skeleton for day 12 (while I'm waiting)

---
+++ 2021/day12.py
@@ -0,0 +1,22 @@
+test_input = """
+
+"""
+
+input = """
+
+"""
+
+def parse_input(data):
+    pass
+
+print("Day 12")
+print("Part 1")
+print("Test input")
+
+
+# print("Puzzle input")
+
+# print("Part 2")
+# print("Test input")
+
+# print("Puzzle input")
