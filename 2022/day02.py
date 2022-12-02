0be9948c19bdc3f6e0e377c822909b5eabc87ac8
Aaron Meurer <asmeurer@gmail.com>
2022-12-02 02:08:35 -0700

    Add day 2 part 2

--- 2022/day02.py
+++ 2022/day02.py
@@ -22,7 +22,7 @@ def parse_input(text):
         games[i] = [mapping[elf], mapping[you]]
     return games
 
-def score(elf, you):
+def score1(elf, you):
     shape_score = you + 1
     if (elf, you) in [(0, 2), (1, 0), (2, 1)]:
         # loss
@@ -35,8 +35,28 @@ def score(elf, you):
         game_score = 6
     return shape_score + game_score
 
+# 0: lose
+# 1: draw
+# 2: win
+
+def score2(elf, you):
+    game_score = you*3
+    if (elf, you) in [(0, 1), (1, 0), (2, 2)]:
+        # rock
+        shape_score = 1
+    elif (elf, you) in [(0, 2), (1, 1), (2, 0)]:
+        # paper
+        shape_score = 2
+    else:
+        # scissors
+        shape_score = 3
+    return shape_score + game_score
+
 def part1(games):
-    return sum(score(elf, you) for elf, you in games)
+    return sum(score1(elf, you) for elf, you in games)
+
+def part2(games):
+    return sum(score2(elf, you) for elf, you in games)
 
 print("Day 2")
 print("Part 1")
@@ -47,3 +67,11 @@ print(part1(test_games))
 print("Puzzle input")
 games = parse_input(input)
 print(part1(games))
+print("Part 2")
+print("Test input")
+test_games = parse_input(test_input)
+print(test_games)
+print(part2(test_games))
+print("Puzzle input")
+games = parse_input(input)
+print(part2(games))
