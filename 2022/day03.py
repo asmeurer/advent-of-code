775a477f09aa9648d7336acf69c45d8b7668b5aa
Aaron Meurer <asmeurer@gmail.com>
2022-12-03 00:35:21 -0700

    Add day 3 part 2

--- 2022/day03.py
+++ 2022/day03.py
@@ -12,6 +12,12 @@ input = open('day03_input').read()
 def parse_input(data):
     return data.strip().splitlines()
 
+def prio(x):
+    if x.islower():
+        return ord(x) - (ord('a') - 1)
+    elif x.isupper():
+        return ord(x) - (ord('A') - 27)
+
 def part1(rucksacks):
     common = []
     for items in rucksacks:
@@ -22,11 +28,13 @@ def part1(rucksacks):
 
     return sum(prio(i) for i in common)
 
-def prio(x):
-    if x.islower():
-        return ord(x) - (ord('a') - 1)
-    elif x.isupper():
-        return ord(x) - (ord('A') - 27)
+def part2(rucksacks):
+    common = []
+    for sack1, sack2, sack3 in zip(rucksacks[0::3], rucksacks[1::3], rucksacks[2::3]):
+        badge = set(sack1).intersection(set(sack2)).intersection(set(sack3))
+        assert len(badge) == 1
+        common.append(badge.pop())
+    return sum(prio(i) for i in common)
 
 print("Day 3")
 print("Part 1")
@@ -35,3 +43,7 @@ test_rucksacks = parse_input(test_input)
 rucksacks = parse_input(input)
 print(part1(test_rucksacks))
 print(part1(rucksacks))
+print("Part 1")
+print("Test input")
+print(part2(test_rucksacks))
+print(part2(rucksacks))
