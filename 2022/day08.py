66d47fc98862c2722447f1538ad10364166a1b55
Aaron Meurer <asmeurer@gmail.com>
2022-12-08 01:30:08 -0700

    Add day 8 part 2

--- 2022/day08.py
+++ 2022/day08.py
@@ -10,6 +10,8 @@ input = open('day08_input').read()
 
 import numpy as np
 
+from math import prod
+
 def parse_input(data):
     return np.array([[int(i) for i in line] for line in
                      data.strip().splitlines()])
@@ -42,8 +44,49 @@ def get_visible(trees):
 
     return visible
 
+def scenic_score(trees, i, j):
+    rows, cols = trees.shape
+
+    height = trees[i, j]
+    scores = []
+
+    score = 0
+    for col in range(j-1, -1, -1):
+        score += 1
+        if trees[i, col] >= height:
+            break
+    scores.append(score)
+
+    score = 0
+    for row in range(i-1, -1, -1):
+        score += 1
+        if trees[row, j] >= height:
+            break
+    scores.append(score)
+
+    score = 0
+    for row in range(i+1, rows):
+        score += 1
+        if trees[row, j] >= height:
+            break
+    scores.append(score)
+
+    score = 0
+    for col in range(j+1, cols):
+        score += 1
+        if trees[i, col] >= height:
+            break
+    scores.append(score)
+
+    return scores
+
 def part1(trees):
     return np.count_nonzero(get_visible(trees))
+
+def part2(trees):
+    rows, cols = trees.shape
+    scores = [scenic_score(trees, i, j) for i in range(rows) for j in range(cols)]
+    return max([prod(score) for score in scores])
 
 print("Day 8")
 print("Part 1")
@@ -55,3 +98,10 @@ print(get_visible(test_trees))
 print(part1(test_trees))
 print("Puzzle input")
 print(part1(trees))
+print("Part 2")
+print("Test input")
+print(scenic_score(test_trees, 1, 2))
+print(scenic_score(test_trees, 3, 2))
+print(part2(test_trees))
+print("Puzzle input")
+print(part2(trees))
