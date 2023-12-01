77756b94f7ef2a7ad0d7dd725cadfc04f1a400a0
Aaron Meurer <asmeurer@gmail.com>
2023-11-30 22:24:57 -0700

    Add day 1 part 2

--- 2023/day01.py
+++ 2023/day01.py
@@ -5,7 +5,19 @@ a1b2c3d4e5f
 treb7uchet
 """.strip()
 
+test_input2 = """
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+""".strip()
+
 input = open('day01_input').read()
+
+import re
 
 def part1(input):
     nums = []
@@ -14,8 +26,38 @@ def part1(input):
         nums.append(int(digits[0] + digits[-1]))
     return sum(nums)
 
+digits = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four' : 4,
+    'five' : 5,
+    'six' : 6,
+    'seven' : 7,
+    'eight' : 8,
+    'nine' : 9,
+    **{str(i): i for i in range(10)}
+}
+
+PATTERN = re.compile('|'.join(digits))
+REV_PATTERN = re.compile(PATTERN.pattern[::-1])
+
+def part2(input):
+    nums = []
+    for line in input.splitlines():
+        first_word = PATTERN.search(line).group()
+        last_word = REV_PATTERN.search(line[::-1]).group()[::-1]
+        num = 10*digits[first_word] + digits[last_word]
+        nums.append(num)
+    return sum(nums)
+
 if __name__ == '__main__':
     print("Day 1")
     print("Part 1")
     print(part1(test_input))
     print(part1(input))
+
+    print("Part 2")
+    print(part2(test_input2))
+    print(part2(input))
