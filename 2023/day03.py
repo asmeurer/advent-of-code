bb7697130bbfa6efa827845e1338721f79ef9c25
Aaron Meurer <asmeurer@gmail.com>
2023-12-03 01:34:31 -0700

    Fix day 3 part 1

--- 2023/day03.py
+++ 2023/day03.py
@@ -27,15 +27,12 @@ class Symbol:
     coord: tuple
     adjacent: list
 
-
-def parse_input(puzzle):
-    return puzzle.splitlines()
-
 def get_numbers_symbols(puzzle):
     numbers = []
     symbols = []
 
-    for i, line in enumerate(puzzle):
+    for i, line in enumerate(puzzle.splitlines()):
+        line += '.'
         num = ''
         for j, c in enumerate(line):
             if c.isdigit():
@@ -54,6 +51,7 @@ def get_numbers_symbols(puzzle):
 
 def part1(puzzle):
     numbers, symbols = get_numbers_symbols(puzzle)
+
     for s in symbols:
         x, y = s.coord
         for n in numbers:
@@ -64,29 +62,9 @@ def part1(puzzle):
                         s.adjacent.append(n)
                         break
 
-    # for s in symbols:
-    #     print(s.value, [n.value for n in s.adjacent])
-    # for n in numbers:
-    #     if not n.adjacent:
-    #         print(n)
-    #         if n.coords[0][1] == 0:
-    #             print(puzzle[n.coords[0][0]-1][n.coords[0][1]:n.coords[-1][1]+2])
-    #             print(puzzle[n.coords[0][0]][n.coords[0][1]:n.coords[-1][1]+2])
-    #             print(puzzle[n.coords[0][0]+1][n.coords[0][1]:n.coords[-1][1]+2])
-    #         else:
-    #             if n.coords[0][0] != 0:
-    #                 print(puzzle[n.coords[0][0]-1][n.coords[0][1]-1:n.coords[-1][1]+2])
-    #             print(puzzle[n.coords[0][0]][n.coords[0][1]-1:n.coords[-1][1]+2])
-    #             print(puzzle[n.coords[0][0]+1][n.coords[0][1]-1:n.coords[-1][1]+2])
-    #         # print(n.value, [s.value for s in n.adjacent])
-
-    # print(sum(n.value for n in numbers))
-    # print(sum(set(n.value for n in numbers if n.adjacent)))
     return sum(n.value for n in numbers if n.adjacent)
 
 if __name__ == '__main__':
-    test_input = parse_input(test_input)
-    puzzle_input = parse_input(puzzle_input)
     print("Day 3")
     print("Part 1")
     print("Test input")
