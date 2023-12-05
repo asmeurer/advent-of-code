b46bae1dbca8b9a65dd5045861a7a419153bdec7
Aaron Meurer <asmeurer@gmail.com>
2023-12-05 01:35:12 -0700

    Work on day 5 part 2

--- 2023/day05.py
+++ 2023/day05.py
@@ -47,6 +47,7 @@ class MapRange:
 @dataclass(unsafe_hash=True)
 class Almanac:
     seeds: list[int] = field(default_factory=list)
+    seed_ranges: list[tuple[int, int]] = field(default_factory=list)
     seed_to_soil: list[MapRange] = field(default_factory=list)
     soil_to_fertilizer: list[MapRange] = field(default_factory=list)
     fertilizer_to_water: list[MapRange] = field(default_factory=list)
@@ -63,6 +64,9 @@ def parse_input(input):
             pass
         elif line.startswith("seeds: "):
             almanac.seeds[:] = [int(s) for s in line.split(':')[1].split()]
+            almanac.seed_ranges[:] = [(almanac.seeds[i], almanac.seeds[i] +
+                                       almanac.seeds[i+1]) for i in
+                                      range(0, len(almanac.seeds) - 1, 2)]
         elif line.startswith("seed-to-soil map:"):
             current_map = almanac.seed_to_soil
         elif line.startswith("soil-to-fertilizer map:"):
@@ -98,6 +102,44 @@ def part1(almanac):
     print(mapped_seeds)
     return min(mapped_seeds)
 
+reverse_map = {
+    "humidity_to_location": "temperature_to_humidity",
+    "temperature_to_humidity": "light_to_temperature",
+    "light_to_temperature": "water_to_light",
+    "water_to_light": "fertilizer_to_water",
+    "fertilizer_to_water": "soil_to_fertilizer",
+    "soil_to_fertilizer": "seed_to_soil",
+    "seed_to_soil": None,
+}
+
+def intersect_intervals(start1, end1, start2, end2):
+    if start1 > end2 or start2 > end1:
+        return None
+    return (max(start1, start2), min(end1, end2))
+
+def search_range(almanac, map_name, start, end):
+    if map_name is None:
+        for seed_range in almanac.seed_ranges:
+            intersection = intersect_intervals(start, end, seed_range[0], seed_range[1])
+            if intersection is not None:
+                yield intersection
+        return
+    mappings = getattr(almanac, map_name)
+    for mapping in sorted(mappings, key=lambda m: m.destination):
+        dest_start, dest_end = mapping.destination, mapping.destination + mapping.length
+        intersection = intersect_intervals(start, end, dest_start, dest_end)
+        if intersection is None:
+            continue
+
+        offset = mapping.destination - mapping.source
+        inverse_start, inverse_end = intersection[0] - offset, intersection[1] - offset
+        yield from search_range(almanac, reverse_map[map_name], inverse_start, inverse_end)
+
+def part2(almanac):
+    for start, end in search_range(almanac, "humidity_to_location", 0, 100000000000):
+        print(start, end)
+
+
 if __name__ == "__main__":
     print("Day 5")
     print("Part 1")
@@ -108,3 +150,8 @@ if __name__ == "__main__":
     print("Puzzle input")
     almanac = parse_input(puzzle_input)
     print(part1(almanac))
+
+    print()
+    print("Part 2")
+    print("Test input")
+    print(part2(test_almanac))
