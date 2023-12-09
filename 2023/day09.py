1f8e5b8282838ae7c6ba8c95f48dbec3fba099e3
Aaron Meurer <asmeurer@gmail.com>
2023-12-08 22:57:02 -0700

    Add day 9 part 2

--- 2023/day09.py
+++ 2023/day09.py
@@ -17,18 +17,27 @@ def parse_input(input):
         sequences.append(np.array([int(i) for i in line.split()]))
     return sequences
 
+def get_poly(sequence):
+    for d in range(1, len(sequence)):
+        if np.all(np.diff(sequence, d) == 0):
+            break
+    return interpolate(sequence[:d], x)
+
 def part1(sequences):
     vals = []
     for sequence in sequences:
-        for d in range(1, len(sequence)):
-            if np.all(np.diff(sequence, d) == 0):
-                break
-        poly = interpolate(sequence[:d], x)
+        poly = get_poly(sequence)
         val = poly.subs(x, len(sequence)+1)
-        # print(val)
-
         vals.append(val)
-    return round(sum(vals))
+    return sum(vals)
+
+def part2(sequences):
+    vals = []
+    for sequence in sequences:
+        poly = get_poly(sequence)
+        val = poly.subs(x, 0)
+        vals.append(val)
+    return sum(vals)
 
 if __name__ == "__main__":
     print("Day 9")
@@ -39,3 +48,8 @@ if __name__ == "__main__":
     print("Puzzle input")
     puzzle_sequences = parse_input(puzzle_input)
     print(part1(puzzle_sequences))
+    print("Part 2")
+    print("Test input")
+    print(part2(test_sequences))
+    print("Puzzle input")
+    print(part2(puzzle_sequences))
