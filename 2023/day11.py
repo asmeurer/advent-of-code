2d9b21ffc02d9d12ca8b28de08b0303d8b4e056b
Aaron Meurer <asmeurer@gmail.com>
2023-12-10 22:36:28 -0700

    Add day 11 part 2

--- 2023/day11.py
+++ 2023/day11.py
@@ -24,20 +24,20 @@ def empty_rows(puzzle):
 def empty_cols(puzzle):
     return np.argwhere(np.all(~puzzle, axis=0)).flatten()
 
-def distance(puzzle, a, b):
+def distance(puzzle, a, b, multiple=2):
     rows = empty_rows(puzzle)
     cols = empty_cols(puzzle)
     xvals = np.searchsorted(rows, [a[0], b[0]])
     yvals = np.searchsorted(cols, [a[1], b[1]])
     return (abs(b[1] - a[1]) + abs(b[0] - a[0]) +
-            abs(xvals[1] - xvals[0]) + abs(yvals[1] - yvals[0]))
+            abs(xvals[1] - xvals[0])*(multiple-1) + abs(yvals[1] - yvals[0])*(multiple-1))
 
-def part1(puzzle):
+def part1_2(puzzle, multiple=1):
     distances = []
     galaxies = np.argwhere(puzzle)
     for i in range(galaxies.shape[0]):
         for j in range(i + 1, galaxies.shape[0]):
-            distances.append(distance(puzzle, galaxies[i], galaxies[j]))
+            distances.append(distance(puzzle, galaxies[i], galaxies[j], multiple=multiple))
 
     return sum(distances)
 
@@ -46,7 +46,15 @@ if __name__ == '__main__':
     print("Part 1")
     print("Test input")
     test_puzzle = parse_input(test_input)
-    print(part1(test_puzzle))
+    print(part1_2(test_puzzle))
     print("Puzzle input")
     puzzle = parse_input(puzzle_input)
-    print(part1(puzzle))
+    print(part1_2(puzzle))
+
+    print("Part 2")
+    print("Test input")
+    print(part1_2(test_puzzle, multiple=10))
+    print(part1_2(test_puzzle, multiple=100))
+
+    print("Puzzle input")
+    print(part1_2(puzzle, multiple=1_000_000))
