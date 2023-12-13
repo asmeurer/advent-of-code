1251535f4791402fb2b2dab6c363fa62ade8c434
Aaron Meurer <asmeurer@gmail.com>
2023-12-12 22:39:47 -0700

    Day 13 part 2

--- 2023/day13.py
+++ 2023/day13.py
@@ -46,6 +46,55 @@ def part1(blocks):
                 break
     return 100*sum(rows) + sum(cols)
 
+
+def part2(blocks):
+    rows = []
+    cols = []
+    for block in blocks:
+        # Find the initial reflection line
+        x, y = block.shape
+        r = c = -1
+        for i in range(1, x):
+            a = block[i:i+min(i, x-i)][::-1]
+            b = block[i-min(i, x-i):i]
+            if np.all(a == b):
+                r = i
+                break
+        for j in range(1, y):
+            a = block[:, j:j+min(j, y-j)][:, ::-1]
+            b = block[:, j-min(j, y-j):j]
+            if np.all(a == b):
+                c = j
+                break
+
+        found = False
+        for idx in np.ndindex(block.shape):
+            bl = block.copy()
+            bl[idx] ^= 1
+            for i in range(1, x):
+                if i == r:
+                    continue
+                a = bl[i:i+min(i, x-i)][::-1]
+                b = bl[i-min(i, x-i):i]
+                if np.all(a == b):
+                    rows.append(i)
+                    found = True
+                    break
+            for j in range(1, y):
+                if j == c:
+                    continue
+                a = bl[:, j:j+min(j, y-j)][:, ::-1]
+                b = bl[:, j-min(j, y-j):j]
+                if np.all(a == b):
+                    cols.append(j)
+                    found = True
+                    break
+            if found:
+                # print(f"smudge at {idx}")
+                # print(rows, cols)
+                break
+    return 100*sum(rows) + sum(cols)
+
 if __name__ == '__main__':
     print("Day 13")
     print("Part 1")
@@ -55,3 +104,8 @@ if __name__ == '__main__':
     print("Puzzle input")
     puzzle_blocks = parse_input(puzzle_input)
     print(part1(puzzle_blocks))
+    print("Part 2")
+    print("Test input")
+    print(part2(test_blocks))
+    print("Puzzle input")
+    print(part2(puzzle_blocks))
