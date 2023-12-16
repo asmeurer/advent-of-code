b8e8277556cee1e3cd39db89d4257ff2c0cf0af0
Aaron Meurer <asmeurer@gmail.com>
2023-12-15 17:32:34 -0700

    Add day 15 part 2

--- 2023/day15.py
+++ 2023/day15.py
@@ -1,6 +1,8 @@
 test_input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
 puzzle_input = open('day15_input').read()
 
+from collections import defaultdict
+
 import numpy as np
 
 def parse_input(input):
@@ -14,6 +16,37 @@ def hash(x):
 def part1(strings):
     return sum(hash(s) for s in strings)
 
+def focusing_power(boxes):
+    return sum((key+1)*(i+1)*focal_length for key, box in boxes.items()
+               for i, (label, focal_length) in enumerate(box))
+
+def part2(strings):
+    boxes = defaultdict(list)
+
+    for s in strings:
+        if '=' in s:
+            label, focal_length = s.split('=')
+            key = hash(label)
+            box = boxes[key]
+            for i in range(len(box)):
+                if box[i][0] == label:
+                    box[i] = (label, int(focal_length))
+                    break
+            else:
+                box.append((label, int(focal_length)))
+        else:
+            assert s.endswith('-')
+            label = s[:-1]
+            key = hash(label)
+            box = boxes[key]
+            for i in range(len(box)):
+                if box[i][0] == label:
+                    del box[i]
+                    break
+
+    # print(boxes)
+    return focusing_power(boxes)
+
 if __name__ == '__main__':
     print("Day 15")
     print("Part 1")
@@ -26,3 +59,9 @@ if __name__ == '__main__':
     print("Puzzle input")
     puzzle_strings = parse_input(puzzle_input)
     print(part1(puzzle_strings))
+
+    print("Part 2")
+    print("Test input")
+    print(part2(test_strings))
+    print("Puzzle input")
+    print(part2(puzzle_strings))
